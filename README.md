d85a6a33efa3d699fef825dfd19222c5cd292713
André Carvalho <afacarvalho@yahoo.com.br>
2017-11-27 23:33:31 -0200

    Some functionality was added as notification alert, change color to rates when variation is up or down. This implementations use a condova plugin that need update:  meteor update

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # bitcointoyou
+This project goal is provide an android app to following price of Bitcoin in BitcoinToYou trader.
+
+Becouse my programming backgroud is the web, more specific in JavaScript, i chosse the Meteor to provide an environment to develop in JavaScript and compile to Android platform.
+
+Make environment to develop
+
+Follow the basic instructions to make the base of this project.
+
+Basic steps:
+
+ curl https://install.meteor.com/ | sh
+ meteor create ~/bitcointoyou
+ meteor 
+ meteor add http
+
+https://www.meteor.com/install
+
+Build and test in Android device
+
+Is pre required to install Android SDK and related tools
+
+```
+meteor add-platform android
+meteor run android-device
+```
+
+Debug on PC
+
+Simple run "meteor" on command line
--- client/main.js
+++ client/main.js
@@ -1,32 +1,86 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor'
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.currentValue = new ReactiveVar(0);
+Template.info.onCreated(function infoOnCreated() {
+  Template.info.values=[];
+  Template.info.maxLength=24;//24 amostras = 2h para 1 amostra a cada 5 minutos
+  Template.info.refreshRate=300;// in seconds
+  Template.info.defaultVarRate=0;
+  Template.info.startingCallAPI();
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().currentValue.get();
-  },
+Template.info.storeLastValue=function(aValue) {
+  if(Template.info.values.length>Template.info.maxLength) {
+    Template.info.values.shift();
+  }
+  Template.info.values.push(aValue);
+  
+};
+
+Template.info.events({
+  'change select'(event) {
+    event.preventDefault();
+    var selectedValue = event.target.value;
+    Template.info.defaultVarRate=selectedValue;
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    var json={};
-    HTTP.call('POST', 'https://www.bitcointoyou.com/API/ticker.aspx', {
-      data: { some: 'json', stuff: 1 }
-    }, (error, result) => {
-      if (!error) {
-        json=JSON.parse(result.content);
-        document.getElementById('currentValue').innerText=json.ticker.last;
-        //instance.currentValue.set(json.ticker.last);
-      }
-    });
-    // increment the counter when button is clicked
-    // instance.currentValue.set(json.currentValue);
-  },
-});
+Template.info.verifyLimiar=function(percent) {
+  var reference=Template.info.defaultVarRate;
+  if(percent>=reference && Meteor.isCordova) {
+    // meteor add cordova:org.apache.cordova.dialogs@0.2.10
+    navigator.notification.beep(2);
+  }
+};
+
+Template.info.calculatingVariation=function() {
+  var first=Template.info.values[0],
+  last=Template.info.values[Template.info.values.length-1],
+  diff=0, ref=0, percent=0,upOrDown='';
+  if(first>last) { // price down
+    ref=first;
+    diff=first-last;
+    upOrDown='-';
+  }else {// price up
+    ref=last;
+    diff=last-first;
+    upOrDown='+';
+  }
+  // ref  === 100%
+  // diff === X%
+  percent=diff*100/ref;
+  percent=percent.toFixed(3);
+  
+  return {variation:upOrDown,percent:+percent};
+};
+
+Template.info.startingCallAPI=function() {
+
+  var json={};
+  HTTP.call('POST', 'https://www.bitcointoyou.com/API/ticker.aspx', {
+    data: {}
+  }, (error, result) => {
+    if (!error) {
+      json=JSON.parse(result.content);
+      json=json.ticker;
+      var buy=+json.buy;
+      buy=buy.toFixed(3);
+      var sell=+json.sell;
+      sell.toFixed(3);
+      document.getElementById('last').innerText=json.last;
+      document.getElementById('high').innerText=json.high;
+      document.getElementById('low').innerText=json.low;
+      document.getElementById('buy').innerText=buy;
+      document.getElementById('sell').innerText=sell;
+      Template.info.storeLastValue(json.last);
+      var values=Template.info.calculatingVariation();
+      Template.info.verifyLimiar(values.percent);// to play beep
+      document.getElementById('percent').innerText=' '+values.variation+values.percent+'%';
+      document.getElementById('percent').style.color=( (values.variation=='+')?('#006600'):('#CC0000') );
+      Meteor.setTimeout(Template.info.startingCallAPI, Template.info.refreshRate*1000);
+    }
+  });
+};
